fc187467fab6613a14b5b483e4a998eb86a64b27
Stealthmate <stealthmate1233@gmail.com>
2016-05-22 13:12:12 +0300

    Fixed CSS mechanics for help and added partial stage 2

--- frontend/scripts/help.js
+++ frontend/scripts/help.js
@@ -1,5 +1,4 @@
-
-var STAGE = 0;
+var STAGE = -1;
 
 
 function help1(done) {
@@ -8,15 +7,23 @@ function help1(done) {
 		return;
 	}
 	
+	if($("#searchpane").attr("collapse")==="true") INFO_FUNCS.escape_f();
+	
 	$("#helppane").attr("stage", "1");
 }
+
+var temp;
 
 function help2(done) {
 	
 	if(done) {
+		KEY_COMMAND_MAP[KEY_ESCAPE].cmd = INFO_FUNCS.escape_f;
 		return;
 	}
 	
+	INFO_FUNCS.escape_f();
+	if(markedMember > 0) INFO_FUNCS.escape_f();
+	KEY_COMMAND_MAP[KEY_ESCAPE].cmd = null;
 	$("#helppane").attr("stage", "2");
 	
 }
@@ -27,6 +34,9 @@ help1, help2
 ]
 
 function advance(n) {
+	
+	if(STAGE == -1) return;
+	
 	HELP_STAGES[STAGE](true);
 	if(n > STAGE) STAGE = n-1;
 	else STAGE++;
@@ -36,14 +46,16 @@ function advance(n) {
 
 function showHelp() {
 	
-	console.log("hello");
+	var stopHelp = $("#helppane").attr("show");
+	if(stopHelp === "true") stopHelp = true;
+	else stopHelp = false;
+	$("#helppane").attr("show", !stopHelp);
 	
-	var val = $("#helppane").attr("show");
-	if(val === "true") val = true;
-	else val = false;
-	$("#helppane").attr("show", !val);
+	if(!stopHelp) STAGE = 0;
+	HELP_STAGES[STAGE](stopHelp);
+	if(stopHelp) STAGE = -1;
 	
-	HELP_STAGES[STAGE](false);
+	$("#helppane").attr("stage", STAGE+1);
 }
 
 HELP_FUNCS = {};
