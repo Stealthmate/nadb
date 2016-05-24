5cdac5706c5e72f5971a8e951a9b32f2763221a7
Stealthmate <stealthmate1233@gmail.com>
2016-05-24 18:18:13 +0300

    Updated scrolling to work with horizontal UI in square mode

--- frontend/scripts/ui/searchpane.js
+++ frontend/scripts/ui/searchpane.js
@@ -1,152 +1,180 @@
 const SUGGESTION_TEMPLATE =
-	"<li" +
-	" onmouseenter=\"SEARCHPANE_FUNCS.selectSuggestionByRef(this);\"" +
-	" onmouseleave=\"SEARCHPANE_FUNCS.deselectSuggestionByRef(this);\"" +
-	" onclick=\"SEARCHPANE_FUNCS.confirmSelected();\" class=\"searchresult\"" +
-	" data-selected=\"false\">" +
-		"<span class=\"searchresult_name\">%name</span>" +
-		"<img class=\"searchresult_avatar\" src=\"resource/images/characters/%id/avatar.jpg\"></img>" +
-	"</li>";
+    "<li" +
+    " onmouseenter=\"SEARCHPANE_FUNCS.selectSuggestionByRef(this);\"" +
+    " onmouseleave=\"SEARCHPANE_FUNCS.deselectSuggestionByRef(this);\"" +
+    " onclick=\"SEARCHPANE_FUNCS.confirmSelected();\" class=\"searchresult\"" +
+    " data-selected=\"false\">" +
+    "<img class=\"searchresult_avatar\" src=\"resource/images/characters/%id/avatar.jpg\"></img>" +
+    "<span class=\"searchresult_name\">%name</span>" +
+    "</li>";
 
 
 var selected_suggestion = 0;
 
 function buildSuggestion(charobj) {
 
-	var t_name = "%name";
-	var t_id = "%id";
+    var t_name = "%name";
+    var t_id = "%id";
 
-	var str = SUGGESTION_TEMPLATE.replace(t_name, charobj.name).replace(t_id, charobj.id);
+    var str = SUGGESTION_TEMPLATE.replace(t_name, charobj.name).replace(t_id, charobj.id);
 
-	return str;
+    return str;
 }
 
 function querySuggestions() {
 
-	var text = $("#searchbox").val();
-	if(text === undefined) text = "";
-	SEARCH_FUNCS.query(text);
+    var text = $("#searchbox").val();
+    if (text === undefined) text = "";
+    SEARCH_FUNCS.query(text);
 }
 
 function populateSuggestions(charobjs) {
 
-	var container = $("#resultlist");
+    var container = $("#resultlist");
 
-	selected_suggestion = 0;
-	container.empty();
-	for(i in charobjs) {
-		container.append(buildSuggestion(charobjs[i]));
-	}
+    selected_suggestion = 0;
+    container.empty();
+    for (i in charobjs) {
+        container.append(buildSuggestion(charobjs[i]));
+    }
 
-	$("#searchbox").focus();
+    $("#searchbox").focus();
 }
 
 function blockNumbers() {
-	if ((event.which >= 49 && event.which <=58)) event.preventDefault();
+    if ((event.which >= 49 && event.which <= 58)) event.preventDefault();
 }
 
 function collapse_searchpane() {
-	if(markedMember > 0) markMemberByNumber(markedMember-1);
-	else {
-		var val = $("#searchpane").attr("collapse");
-		if(val === "true") val = true;
-		else val = false;
-		$("#searchpane").attr("collapse", !val);
-
-		if(!val) $("#searchbox").blur();
-		else $("#searchbox").focus();
-	}
+    if (markedMember > 0) markMemberByNumber(markedMember - 1);
+    else {
+        var val = $("#searchpane").attr("collapse");
+        if (val === "true") val = true;
+        else val = false;
+        $("#searchpane").attr("collapse", !val);
+    }
 }
 
+function refocus() {
+    shouldFocus = ($("#searchpane").attr("collapse") === "false");
+    if (shouldFocus) $("#searchbox").focus();
+    else $("#searchbox").blur();
+}
 
 function shouldCapture() {
-	return $("#searchpane").attr("collapse") == "false";
+    return $("#searchpane").attr("collapse") == "false";
 }
 
 function selectByRef(self) {
 
-	if(!shouldCapture()) return;
+    if (!shouldCapture()) return;
 
-	var i = $(".searchresult").index(self);
-	if(selected_suggestion > 0) $($(".searchresult").get(selected_suggestion-1)).attr("data-selected", "false");
-	selectSuggestion(i+1);
-	selected_suggestion = i+1;
+    var i = $(".searchresult").index(self);
+    if (selected_suggestion > 0) $($(".searchresult").get(selected_suggestion - 1)).attr("data-selected", "false");
+    selectSuggestion(i + 1);
+    selected_suggestion = i + 1;
 }
 
 function deselectByRef(self) {
 
-	if(!shouldCapture()) return;
+    if (!shouldCapture()) return;
 
-	deselectByID($(".searchresult").index(self)+1);
-	selected_suggestion = 0;
+    deselectByID($(".searchresult").index(self) + 1);
+    selected_suggestion = 0;
 }
 
 function deselectByID(n) {
-	$($(".searchresult").get(n-1)).attr("data-selected", "false");
+    $($(".searchresult").get(n - 1)).attr("data-selected", "false");
 }
 
 function selectSuggestion(n) {
-	var elem = $(".searchresult").get(n-1);
-	$(elem).attr("data-selected", "true");
+    var elem = $(".searchresult").get(n - 1);
+    $(elem).attr("data-selected", "true");
 }
 
-function scrollBy(n) {
-	$("#resultlist").scrollTop($("#resultlist").scrollTop() + ($(".searchresult").outerHeight()*n));
+function scrollVerticallyBy(n) {
+    $("#resultlist").scrollTop($("#resultlist").scrollTop() + ($(".searchresult").outerHeight() * n));
+}
+
+function scrollHorizontallyBy(n) {
+    $("#resultlist").scrollLeft($("#resultlist").scrollLeft() + ($(".searchresult").outerWidth() * n));
 }
 
 function scrollIfNeeded() {
-	var view_h = $("#resultlist").height();
-	var li_h = $(".searchresult").outerHeight();
+    if ($("#resultlist").css("column-count") == "4") {
+        var view_w = $("#resultlist").width();
+        var li_w = $(".searchresult").outerWidth();
+        var max_li = (Math.floor(view_w / li_w)) * 4;
+        var current_first = Math.floor($("#resultlist").scrollLeft() * 4 / li_w) + 1;
 
-	var max_li = Math.floor(view_h/li_h) - 1;
+		var diff = current_first - selected_suggestion - 1;
 
-	var current_top = Math.floor($("#resultlist").scrollTop()/li_h)+1;;
+		var scroll_amount = 0;
+		if(diff < 0) scroll_amount = -Math.floor((diff + max_li)/ max_li)*4;
+		else if (diff >= 0) scroll_amount = (Math.floor(-diff/max_li) - 1) * max_li/4;
+		scrollHorizontallyBy(scroll_amount);
+        return;
+    }
 
-	if(selected_suggestion < current_top) scrollBy(selected_suggestion - current_top);
-	if(selected_suggestion > current_top + max_li) scrollBy(selected_suggestion - current_top - max_li);
+    var view_h = $("#resultlist").height();
+    var li_h = $(".searchresult").outerHeight();
+
+    var max_li = Math.floor(view_h / li_h) + 1;
+
+    var current_first = Math.floor($("#resultlist").scrollTop() / li_h) + 1;
+	var diff = current_first - selected_suggestion - 1;
+
+	var scroll_amount = 0;
+	console.log(diff);
+	console.log("Max " + max_li)
+	if(diff < 0) scroll_amount = -Math.floor((diff + max_li)/ max_li)*max_li;
+	else if (diff >= 0) scroll_amount = (Math.floor(-diff/max_li) - 1) * max_li;
+	scrollVerticallyBy(scroll_amount);
 }
 
 function selectNext() {
 
-	if(!shouldCapture()) return;
+    if (!shouldCapture()) return;
 
-	var elemlist = $(".searchresult");
-	if(elemlist.length == 0) return;
+    var elemlist = $(".searchresult");
+    if (elemlist.length == 0) return;
 
-	deselectByID(selected_suggestion);
+    deselectByID(selected_suggestion);
 
-	if(selected_suggestion == elemlist.length) selected_suggestion = 1;
-	else selected_suggestion = selected_suggestion + 1;
+    if (selected_suggestion == elemlist.length) selected_suggestion = 1;
+    else selected_suggestion = selected_suggestion + 1;
 
-	selectSuggestion(selected_suggestion);
-	scrollIfNeeded();
+    selectSuggestion(selected_suggestion);
+    scrollIfNeeded();
 
 }
 
 function selectPrev() {
 
-	if(!shouldCapture()) return;
+    if (!shouldCapture()) return;
 
-	var elemlist = $(".searchresult");
-	if(elemlist.length == 0) return;
-	deselectByID(selected_suggestion);
+    var elemlist = $(".searchresult");
+    if (elemlist.length == 0) return;
+    deselectByID(selected_suggestion);
 
-	if(selected_suggestion <= 1) selected_suggestion = elemlist.length;
-	else selected_suggestion = selected_suggestion - 1;
+    if (selected_suggestion <= 1) selected_suggestion = elemlist.length;
+    else selected_suggestion = selected_suggestion - 1;
 
-	selectSuggestion(selected_suggestion);
-	scrollIfNeeded();
+    selectSuggestion(selected_suggestion);
+    scrollIfNeeded();
 }
 
 function confirmSelect() {
 
-	if(!shouldCapture()) return;
+    if (!shouldCapture()) return;
 
-	TEAMSPANE_FUNCS.setMarkedMember(charobjs[selected_suggestion-1]);
+    TEAMSPANE_FUNCS.setMarkedMember(charobjs[selected_suggestion - 1]);
 }
 
 var SEARCHPANE_FUNCS = {};
 SEARCHPANE_FUNCS.collapse = collapse_searchpane;
+SEARCHPANE_FUNCS.refocus = refocus;
+$("#searchpane").on("transitionend", refocus);
 
 SEARCHPANE_FUNCS.query = querySuggestions;
 
